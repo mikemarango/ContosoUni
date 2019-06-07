462f599055a79ed22d978356740499758f1a913c
Michael Marango <mike.marango@gmail.com>
2019-06-07 11:08:38 -0700

    Updated the create and edit

--- ContosoUniversity/ContosoUniversity_Razor/Pages/Students/Create.cshtml.cs
+++ ContosoUniversity/ContosoUniversity_Razor/Pages/Students/Create.cshtml.cs
@@ -30,10 +30,19 @@ namespace ContosoUniversity_Razor.Pages.Students
                 return Page();
             }
 
-            _context.Student.Add(Student);
-            await _context.SaveChangesAsync();
+            var emptyStudent = new Student();
 
-            return RedirectToPage("./Index");
+            if (await TryUpdateModelAsync(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Student.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+
+            return null;
         }
     }
 }
