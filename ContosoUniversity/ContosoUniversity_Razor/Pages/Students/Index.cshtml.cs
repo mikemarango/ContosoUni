02af586c45d7bee1358d72b8a1420bd0cc59aa0d
Michael Marango <mike.marango@gmail.com>
2019-06-09 08:13:22 -0700

    Added sorting

--- ContosoUniversity/ContosoUniversity_Razor/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/ContosoUniversity_Razor/Pages/Students/Index.cshtml.cs
@@ -20,10 +20,36 @@ namespace ContosoUniversity_Razor.Pages.Students
         }
 
         public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Student.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentIQ = from s in _context.Student
+                                            select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentIQ = studentIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentIQ = studentIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Student = await studentIQ.AsNoTracking().ToListAsync();
         }
     }
 }
