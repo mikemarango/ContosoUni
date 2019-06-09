77817fee51633e37ffa6ea83bb2ea32310b612eb
Michael Marango <mike.marango@gmail.com>
2019-06-09 10:58:23 -0700

    handled complex data model

--- ContosoUniversity/ContosoUniversity_Razor/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity_Razor/Data/SchoolContext.cs
@@ -13,5 +13,21 @@ namespace ContosoUniversity_Razor.Data
         public DbSet<Student> Student { get; set; }
         public DbSet<Enrollment> Enrollment { get; set; }
         public DbSet<Course> Course { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
