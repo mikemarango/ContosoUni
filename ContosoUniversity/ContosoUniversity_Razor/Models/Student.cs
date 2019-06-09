0fe968eaac31c88652a227347ad6f4a1d34d37db
Michael Marango <mike.marango@gmail.com>
2019-06-09 09:13:20 -0700

    Student entity update

--- ContosoUniversity/ContosoUniversity_Razor/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity_Razor/Models/Student.cs
@@ -8,15 +8,20 @@ namespace ContosoUniversity_Razor.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
         [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-
+        public string FullName => $"{LastName} {FirstMidName}";
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
