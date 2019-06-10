32897a92602b60161ea7b1c3e74e8fc0c0e68066
Michael Marango <mike.marango@gmail.com>
2019-06-10 01:37:48 -0700

    Added courses by instructor

--- ContosoUniversity/ContosoUniversity_Razor/Pages/Instructors/Index.cshtml.cs
+++ ContosoUniversity/ContosoUniversity_Razor/Pages/Instructors/Index.cshtml.cs
@@ -21,21 +21,35 @@ namespace ContosoUniversity_Razor.Pages.Instructors
 
         public InstructorIndexData Instructor { get; set; }
         public int InstructorID { get; set; }
+        public int CourseID { get; set; }
 
-        public async Task OnGetAsync(int? id)
+        public async Task OnGetAsync(int? id, int? courseID)
         {
-            Instructor = new InstructorIndexData();
-            Instructor.Instructors = await _context.Instructors
+            Instructor = new InstructorIndexData
+            {
+                Instructors = await _context.Instructors
                   .Include(i => i.OfficeAssignment)
                   .Include(i => i.CourseAssignments)
                     .ThenInclude(i => i.Course)
+                    .ThenInclude(i => i.Department)
                   .AsNoTracking()
                   .OrderBy(i => i.LastName)
-                  .ToListAsync();
+                  .ToListAsync()
+            };
 
             if (id != null)
             {
                 InstructorID = id.Value;
+                Instructor instructor = Instructor.Instructors.Where(
+                            i => i.ID == id.Value).Single();
+                Instructor.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if (courseID != null)
+            {
+                CourseID = courseID.Value;
+                Instructor.Enrollments = Instructor.Courses.Where(
+                    x => x.CourseID == courseID).Single().Enrollments;
             }
         }
     }
