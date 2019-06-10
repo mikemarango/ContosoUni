a3c5ecbeec4f8cf640341ccd2fa5b163a0525f0d
Michael Marango <mike.marango@gmail.com>
2019-06-10 06:49:19 -0700

    Scaffolded department and modified index

---
+++ ContosoUniversity/ContosoUniversity_Razor/Pages/Departments/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity_Razor.Data;
+using ContosoUniversity_Razor.Models;
+
+namespace ContosoUniversity_Razor.Pages.Departments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public IndexModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Department> Department { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Department = await _context.Departments
+                .Include(d => d.Administrator).ToListAsync();
+        }
+    }
+}
